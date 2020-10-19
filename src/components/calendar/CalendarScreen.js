f1637176b75bb16bd29fd10be69abfdb0b836cea
Mauricio Cartagena <mc973220@gmail.com>
2020-10-18 23:05:33 -0400

    se agrego el modal al proyecto

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment';
 
 import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages-es';
+import { CalendarEvent } from './CalendarEvent';
+import { CalendarModal } from './CalendarModal';
+
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es';
+
+//change spanish
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);// or globalizeLocalizer
 
@@ -12,24 +20,72 @@ const events = [{
     title: 'Mi Cumpleaños',
     start: moment().toDate(), //new Date()
     end: moment().add( 2, 'hours' ).toDate(),
-    bgcolor:"#fafafa"
+    bgcolor:"#fafafa",
+    notes:'send Gmail',
+    user:{
+      _id: '123',
+      name: 'Mauricio'
+    }
 
 }];
 
 export const CalendarScreen = () => {
-    return (
+
+  const [lastView, setLastView] = useState( localStorage.getItem('lastView') || 'month' )
+
+  const onDoubleClick = (e) => {
+
+    console.log(e);
+
+  }
+  const onSelectEvent= (e) => {
+
+    console.log(e);
+
+  }
+
+  const onViewChange = (e) => {
+    setLastView(e);
+    localStorage.setItem('lastView', e);
+  }
+  
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius:'0px',
+      opacity: 0.8,
+      display: 'block',
+      color:'white'
+    }
+
+    return{
+       style
+    }
+
+  };
+
+  return (
         <div className="calendar-screen" >
           <Navbar/>
-
-
-
 
           <Calendar
             localizer={ localizer }
             events={ events }
             startAccessor="start"
             endAccessor="end"
+            messages={ messages }
+            eventPropGetter={ eventStyleGetter } 
+            onDoubleClickEvent={ onDoubleClick }
+            onSelectEvent={ onSelectEvent }
+            onView={ onViewChange }
+            view={ lastView }
+            components={{
+              event:CalendarEvent
+            }}
           />
+
+          <CalendarModal/>
         </div>
     )
 }
---
+++ src/helpers/calendar-messages-es.js
@@ -0,0 +1,19 @@
+
+
+export const messages = {
+
+    allDay: 'Todo el dia',
+    previous: '<',
+    next: '>',
+    today: 'Hoy',
+    month: 'Mes',
+    week: 'Semana',
+    day: 'Dia',
+    agenda: 'Agenda',
+    date: 'Fecha',
+    time: 'Hora',
+    event: 'Evento',
+    noEventsInRange: 'No ahy eventos en este rango',
+    showMore: total => `+ Ver mas (${ total })`
+    
+};
