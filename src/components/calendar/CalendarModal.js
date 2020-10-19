698f8d7acb724e39b89f77c68a56d51a4134930b
Mauricio Cartagena <mc973220@gmail.com>
2020-10-19 02:12:51 -0400

    added events forn

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,5 +1,9 @@
+import moment from 'moment';
 import React, { useState } from 'react';
+import DateTimePicker from 'react-datetime-picker';
+
 import Modal from 'react-modal'; 
+
 
 
 const customStyles = {
@@ -15,26 +19,137 @@ const customStyles = {
 
 Modal.setAppElement('#root');
 
+const now = moment().minutes(0).seconds(0).add( 1, 'hours');
+const last = now.clone().add(1, 'hours' );
+
 
 export const CalendarModal = () => {
 
-    const [isOpen, setIsOpen] = useState( true );
+  const [ dateStart, setDateStart] = useState( now.toDate() );
+  const [ dateEnd, setDateEnd] = useState( last.toDate() );
 
-    const closeModal = () => {
-         setIsOpen( false );
-    }
-    return (
-        <Modal
-          isOpen={ isOpen }
-        //   onAfterOpen={afterOpenModal}
-          onRequestClose={ closeModal }
-          style={customStyles}
-          contentLabel="Example Modal"
+  const [formValues, setFormValues] = useState({
+
+      title:'Evento',
+      notes:'',
+      start: now.toDate(),
+      end: last.toDate()  
+  });
+
+  const { notes, title } = formValues;
+
+  const handleInputChange = ({ target }) => {
+
+    setFormValues({
+      ...formValues,
+      [target.name]:target.value
+    });
+
+  }
+
+  const closeModal = () => {
+    
+  }
+
+  const handleStartDateChange = ( e ) =>{
+
+    setDateStart( e );
+    setFormValues({
+      ...formValues,
+      end:e
+    });
+
+  }
+  
+  const handleEndDateChange = ( e ) => {
+    setDateEnd(e);
+    setFormValues({
+      ...formValues,
+      last:e
+    });
+  }
+
+  const handleSubmitForm = ( e ) => {
+
+    e.preventDefault();
+
+    console.log(formValues);
+
+  }
+
+  return (
+      <Modal
+        isOpen={ true }
+      //   onAfterOpen={afterOpenModal}
+        onRequestClose={ closeModal }
+        style={customStyles}
+        className="modal"
+        closeTimeoutMS={ 200 }
+        overlayClassName="modal-fondo"
+      >
+          <h1> Nuevo evento </h1>
+      <hr />
+        <form 
+              className="container"
+              onSubmit={ handleSubmitForm }
         >
-            <h1> Hola Mundo </h1>
-            <hr/>
-            <span> hola de nuevo </span>
 
-        </Modal>
-    )
+            <div className="form-group">
+                <label>Fecha y hora inicio</label>
+                <DateTimePicker
+                  onChange={ handleStartDateChange }
+                  value={ dateStart}
+                  className="form-control"
+                />
+            </div>
+
+            <div className="form-group">
+                <label>Fecha y hora fin</label>
+                <DateTimePicker
+                  onChange={ handleEndDateChange }
+                  value={ dateEnd}
+                  minDate={ dateStart }
+                  className="form-control"
+                />
+            </div>
+
+            <hr />
+            <div className="form-group">
+                <label>Titulo y notas</label>
+                <input 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Título del evento"
+                    name="title"
+                    autoComplete="off"
+                    value={ title }
+                    onChange={ handleInputChange }
+                />
+                <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+            </div>
+
+            <div className="form-group">
+                <textarea 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Notas"
+                    rows="5"
+                    name="notes"
+                    value={ notes }
+                    onChange={ handleInputChange }
+                ></textarea>
+                <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+            </div>
+
+            <button
+                type="submit"
+                className="btn btn-outline-primary btn-block"
+            >
+                <i className="far fa-save"></i>
+                <span> Guardar</span>
+            </button>
+
+        </form>
+      </Modal>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CalendarApp } from './CalendarApp';
-import './styles/bootstrap.min.css';
 
 import './styles/styles.css'; 
 
